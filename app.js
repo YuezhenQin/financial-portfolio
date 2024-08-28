b7366be60f4db30c3b0410b9f1dd814d0d7a8167
LKUGB <jackjin@live.com>
2024-08-28 02:35:40 +0000

    merged changes with crypto coin fetcher

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import financialRoutes from './app/routes/financialRoute.js';
+import cryptoRoutes from './app/routes/cryptocoinRoute.js'
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 app.use('/oneInvest', financialRoutes);
+app.use('/crypto', cryptoRoutes);
 
 export default app;
