8c6324671a03c9d220d512ac079d9e7feba51760
song <sxxsdu@163.com>
2024-08-30 02:59:41 +0000

    devops script and sql

--- deploy.sh
+++ deploy.sh
@@ -33,17 +33,24 @@ npm test > /dev/null || { echo "${red}Tests failed for financial-portfolio-backe
 
 echo "🚀 ${bold}Starting the application backend...${normal}"
 npm start &
-frontend_pid=$! 
+backend_pid=$! 
 
-# # Go back to the root directory
-# cd .. || { echo "Failed to return to the root directory"; exit 1; }
+# Go back to the root directory
+cd .. || { echo "Failed to return to the root directory"; exit 1; }
 
-# # Install, test, and start for financial-portfolio-backend
-# echo "⏳ ${bold}Handling financial-portfolio-backend...${normal}"
-# cd financial-portfolio-backend || { echo "Failed to enter music-front-end directory"; exit 1; }
+# Set the repository URL
+REPO_URL_FRONT="https://github.com/YuezhenQin/financial-portfolio-frontend.git"
 
-# echo "Installing dependencies... please wait"
-# npm install > /dev/null || { echo "Failed to install dependencies for music-front-end"; exit 1; }
+# Clone the repository
+echo "📦 Cloning the repository from ${bold} $REPO_URL_FRONT...${normal}"
+git clone $REPO_URL_FRONT > /dev/null || { echo "Failed to clone repository"; exit 1; }
+
+# Install, test, and start for financial-portfolio-frontend
+echo "⏳ ${bold}Handling financial-portfolio-frontend...${normal}"
+cd financial-portfolio-frontend || { echo "Failed to enter financial-portfolio-frontend directory"; exit 1; }
+
+echo "Installing dependencies... please wait"
+npm install > /dev/null || { echo "Failed to install dependencies for financial-portfolio-frontend"; exit 1; }
 
 # if [ -d "__test__" ]; then
 #     echo "Running tests..."
@@ -53,20 +60,20 @@ frontend_pid=$!
 # fi
 # echo "🚀 ${bold}Starting the application..."${normal}
 # npm start &
-# backend_pid=$! 
+# frontend_pid=$! 
 
-# # Wait for both background processes to complete
-# sleep 2
+# Wait for both background processes to complete
+sleep 2
 
-# echo "✅${green}${bold}Deployment completed successfully.${normal}"
+echo "✅${green}${bold}Deployment completed successfully.${normal}"
 
-# echo 'Press any touch to stop all servers'
-# read -n 1 -s  # Wait for user input
+echo 'Press any touch to stop all servers'
+read -n 1 -s  # Wait for user input
 
-# echo "Stopping the app"
+echo "Stopping the app"
 # kill $frontend_pid > /dev/null
-# kill $backend_pid > /dev/null
+kill $backend_pid > /dev/null
 
-# wait
+wait
 
-# echo -e "\n \n ${bold}Done, a bientôt! 👋 ${normal}"
+echo -e "\n \n ${bold}Done, See u next time!!! 👋 ${normal}"
